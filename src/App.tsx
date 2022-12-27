4983013e6cf4d5a2ed21d903f370f1b64a3dd2c0
Patrick Mota <58093259+patrickmps@users.noreply.github.com>
2022-12-27 20:57:17 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import {Title} from './styles/styles'
+import GlobalStyle from './styles/global'
+
+function App() {
+  return (
+    <div className='App'>
+      <GlobalStyle />
+      <Title>Portfólio</Title>
+    </div>
+  );
+}
+
+export default App
---
+++ src/styles/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components"
+
+export const Title = styled.h1`
+  font-size: 1.5em;
+  font-weight: 600;
+  text-align: center;
+  color: #FFF;
+`;
